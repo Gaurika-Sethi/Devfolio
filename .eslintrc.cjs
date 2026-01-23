ecc8e13870dffce93117e75c592ef21ef258fdbc
Gaurikasethi <gaurikasethi88@gmail.com>
2026-01-23 12:26:11 +0530

    initial setup complete

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  ignorePatterns: [
+    "node_modules/",
+    "dist/",
+    "build/",
+    ".next/",
+    ".turbo/",
+  ],
+};
---
+++ apps/api/src/models/workspace.ts
@@ -0,0 +1,5 @@
+export interface Workspace {
+  workspaceId: string;
+  repoPath: string;
+  createdAt: number;
+}
